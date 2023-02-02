a063af5a184f44ce3a124650a48ffe1eeac7fee9
Lurgia9072 <i202023015@cibertec.edu.pe>
2023-02-01 19:36:16 -0500

    configurations

---
+++ to-share-fvm/lib/lens/graphql/apollo-client.ts
@@ -0,0 +1,70 @@
+import {
+  ApolloClient,
+  ApolloLink,
+  DefaultOptions,
+  from,
+  HttpLink,
+  InMemoryCache,
+} from "@apollo/client/core";
+import { onError } from "@apollo/client/link/error";
+import { getFromLocalStorage } from "../localStorage";
+
+const API_URL = "https://api.lens.dev";
+let authenticationToken: string | null = null;
+
+const defaultOptions: DefaultOptions = {
+  watchQuery: {
+    fetchPolicy: "no-cache",
+    errorPolicy: "ignore",
+  },
+  query: {
+    fetchPolicy: "no-cache",
+    errorPolicy: "all",
+  },
+};
+
+export const setAuthenticationToken = (token: string | null) => {
+  authenticationToken = token;
+};
+
+export const getAuthenticationToken = () => {
+  if (authenticationToken) {
+    return authenticationToken;
+  }
+  const localStorageProfile = getFromLocalStorage();
+  return localStorageProfile?.accessToken;
+};
+
+const httpLink = new HttpLink({
+  uri: API_URL,
+  fetch,
+});
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    );
+
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+
+const authLink = new ApolloLink((operation, forward) => {
+  const token = getAuthenticationToken();
+
+  operation.setContext({
+    headers: {
+      "x-access-token": token ? `Bearer ${token}` : "",
+    },
+  });
+
+  return forward(operation);
+});
+
+export const apolloClient = new ApolloClient({
+  link: from([errorLink, authLink, httpLink]),
+  cache: new InMemoryCache(),
+  defaultOptions: defaultOptions,
+});
---
+++ to-share-fvm/pages/_app.tsx
@@ -0,0 +1,69 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
+import LensAuthenticationProvider from "../components/LensAuthenticationProvider";
+import TagsFilterProvider from "../components/TagsFilterProvider";
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { mainnet, polygon, optimism, arbitrum, goerli } from 'wagmi/chains';
+
+ /* const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+} */
+
+//export const theme = extendTheme({ colors })
+
+const { chains, provider, webSocketProvider } = configureChains(
+  [
+    mainnet,
+    polygon,
+    optimism,
+    arbitrum,
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [goerli] : []),
+  ],
+  [
+    alchemyProvider({
+      // This is Alchemy's default API key.
+      // You can get your own at https://dashboard.alchemyapi.io
+
+      apiKey: 'J4w7wEYMXrFo0oelBhM1w4somBQlDQAh',
+      //apiKey: '',
+
+    }),
+    publicProvider(),
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "My RainbowKit App",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
+
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <LensAuthenticationProvider>
+        <RainbowKitProvider chains={chains}>
+        <TagsFilterProvider>
+          <Component {...pageProps} />
+        </TagsFilterProvider>
+        </RainbowKitProvider>
+      </LensAuthenticationProvider>
+    </WagmiConfig>
+  );
+}
+
+export default App;
